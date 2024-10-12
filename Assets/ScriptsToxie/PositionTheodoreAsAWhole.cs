f0cfadac313843ece12b91a3f72de92b78058fe4
toxie <gamepiatnitza@gmail.com>
2024-10-12 17:53:52 +0300

    teodoriksas(arturas myli vyrus <3)

---
+++ Assets/ScriptsToxie/PositionTheodoreAsAWhole.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionTheodoreAsAWhole : MonoBehaviour
+{
+    [SerializeField] GameObject TheodoreHandLeft;
+    [SerializeField] GameObject TheodoreHandRight;
+    [SerializeField] float speed = 0.1f;       // Speed of the up/down movement
+    [SerializeField] float distance = 0.5f;  // The distance to move up and down
+
+    void Update()
+    {
+        // Calculate the vertical oscillation for the hands
+        float offsetY = Mathf.PingPong(Time.time * speed, distance * 2) - distance;
+
+        // Set the hands' positions relative to Theodore's current position, applying vertical movement
+        TheodoreHandRight.transform.position = new Vector3(transform.position.x + 0.85f, transform.position.y + offsetY, transform.position.z);
+        TheodoreHandLeft.transform.position = new Vector3(transform.position.x - 0.85f, transform.position.y + offsetY, transform.position.z);
+    }
+}
